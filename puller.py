3d632b1623a23bae7ce4956a230ea7cbc38501ea
sjgoebel <sgoebel@gmail.com>
2021-11-09 19:01:30 -0600

    update for macos

--- puller.py
+++ puller.py
@@ -9,8 +9,8 @@ import time
 import os
 
 def get_projects():
-    if not os.path.isdir("/home/ubuntu/studentwork/"):
-        print("Make a directory called 'studentwork' in your workspace.")
+    if not os.path.isdir("~/Documents/studentwork/"):
+        print("Make a directory called 'studentwork' in your Documents folder.")
         return
 
     parser = optparse.OptionParser()
@@ -33,7 +33,7 @@ def get_projects():
         return
 
     #make the assignment folder.  If it already exists, make a new one with today's date appended.
-    subprocess.call("mkdir /home/ubuntu/studentwork/" + foldername, shell=True)
+    subprocess.call("mkdir ~/Documents/studentwork/" + foldername, shell=True)
 
 
     people = file.readlines()
@@ -42,7 +42,7 @@ def get_projects():
         githubname += ".git"
         #git clone the folder.
         githuburl = "git@github.com:me50/" + githubname
-        destinationurl = "/home/ubuntu/studentwork/" + foldername + "/" + studentname
+        destinationurl = "~/Documents/studentwork/" + foldername + "/" + studentname
         pexpecter = pexpect.spawn("git clone -b " + options.branch + " " + githuburl + " " + destinationurl)
         pexpecter.wait()
 
